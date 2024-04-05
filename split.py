f24a10e24054642b02dfb86d065202084679d131
Judy Wangari <142023348+Wangari-j@users.noreply.github.com>
2024-04-05 16:40:35 +0300

    Add files via upload

---
+++ split.py
@@ -0,0 +1,17 @@
+name = str(input("Enter your name: "))
+
+age = int(input("Enter your age: "))
+
+residence = str(input("Enter your place of residence: "))
+
+print(name + " is a kikuyu name that means 'resilient' and 'grit' . ")
+
+longSentence = "My name is Judy and I don't like love songs "
+
+
+#split
+# judy -> j.u.d.y
+print(longSentence.split())
+print(residence.split())
+
+
